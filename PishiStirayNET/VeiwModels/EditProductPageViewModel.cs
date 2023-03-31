9dda9322a9157cb8be11154a0690ac34847a84c5
Lazurigost <lazurigost@gmail.com>
2023-03-31 12:44:07 +0500

    bruh

--- PishiStirayNET/VeiwModels/EditProductPageViewModel.cs
+++ PishiStirayNET/VeiwModels/EditProductPageViewModel.cs
@@ -1,12 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using PishiStiray.Infrastructure;
 using PishiStiray.Services;
-using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PishiStiray.VeiwModels
 {
