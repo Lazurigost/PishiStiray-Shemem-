10a1e2c1e2f3a82b1938cd138658627f0c6cbe31
Lazurigost <lazurigost@gmail.com>
2023-05-19 12:31:03 +0500

    How to get da points holy moly

---
+++ PishiStirayNET/Models/Order.cs
@@ -0,0 +1,26 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using PishiStiray.Models.DbEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PishiStiray.Models
+{
+    public partial class Order : Orderuser
+    {
+        //[ObservableProperty]
+        //private float fullPrice;
+        //[ObservableProperty]
+        //private float discount;
+        //public float? Price
+        //{
+        //    get => FullPrice - Discount;
+        //}
+        //[ObservableProperty]
+        //private List<int> productQuantities;
+        //[ObservableProperty]
+        //private List<ProductDB> products;
+    }
+}
---
+++ PishiStirayNET/Services/DocumentService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PishiStiray.Services
+{
+    public class DocumentService
+    {
+        private readonly TradeContext _tradeContext;
+
+        public DocumentService(TradeContext tradeContext)
+        {
+            _tradeContext = tradeContext;
+        }
+
+        //public async void CreateDocument(Order order, string path)
+    }
+}
